1ae37b1acd9dc5049498cc70ed09e756f11db0c3
phawit-s <56498877+phawit-s@users.noreply.github.com>
2022-05-10 17:17:32 +0700

    Add : permetheus

--- back-end/index.js
+++ back-end/index.js
@@ -6,6 +6,23 @@ const port = 4000;
 
 app.use(express.json())
 // app.use(express.urlencoded())
+const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus');
+const { MeterProvider }  = require('@opentelemetry/sdk-metrics-base');
+
+// Add your port and startServer to the Prometheus options
+const options = {port: 9464, startServer: true};
+const exporter = new PrometheusExporter(options);
+
+// Register the exporter
+const meter = new MeterProvider({
+  exporter,
+  interval: 1000,
+}).getMeter('demo-prometheus');
+
+// Now, start recording data
+const counter = meter.createCounter('count_database', {
+  description: 'count database'
+});
 
 const mongoose = require("mongoose");
 const database = require("./database/database");
@@ -23,6 +40,10 @@ app.use(cors());
 
 //ทดลอง Database
 app.post('/database', async (req, res) => {
+    counter.add(1, { service_name: "testservice" });
+    counter.add(1, { service_name: "testservice", status: "success", path: "/database" });
+    counter.add(1, { service_name: "testservice", status: "failure", path: "/database" });
+    counter.add(1, { service_name: "testservice", status: "data not found", path: "/database" });
     try {
         let a = new Blog({
             vaccinationcenter: "สถานีกลางบางซือ",
@@ -68,9 +89,9 @@ app.get("/", (req, res) => {
 
 app.get('/test', async function (req, res) {
     counter.add(1, { service_name: "testservice" });
-    counter.add(1, { service_name: "testservice", status: "success", path: "/test" });
-    counter.add(1, { service_name: "testservice", status: "failure", path: "/test" });
-    counter.add(1, { service_name: "testservice", status: "data not found", path: "/test" });
+    counter.add(1, { service_name: "testservice", status: "success", path: "/database" });
+    counter.add(1, { service_name: "testservice", status: "failure", path: "/database" });
+    counter.add(1, { service_name: "testservice", status: "data not found", path: "/database" });
 
     res.type('json')
     var delay = Math.floor((Math.random() * 2000) + 100);
