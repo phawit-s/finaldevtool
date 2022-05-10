130f60b572567da3015b429b97fdc055b7b3a965
phawit-s <56498877+phawit-s@users.noreply.github.com>
2022-05-10 15:03:01 +0700

    Add : gitignore and dockerignore

--- back-end/models/models.js
+++ back-end/models/models.js
@@ -4,6 +4,6 @@ const blogSchema = new mongoose.Schema({
     title: String,
 });
 
-const Blog = mongoose.model("devtooltables", blogSchema);
+const Blog = mongoose.model("registerdevtools", blogSchema);
 
 module.exports = Blog
